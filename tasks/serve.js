3a2d893815341ed7be7b23d1006fc7191ec71bf5
Peter Mallett <petermallett@users.noreply.github.com>
2022-10-14 11:10:05 -0600

    Testing container (#5)
    * build(): do not copy node_modules into image
    
    * build(): fix node_modules copy out to host
    
    * chore(): Clean pkg cache, include component build helper
    
    * build(): copy out the new package-lock after npm i
    
    * chore(): Return the registry and integrity hash.
    
    * Make dev index html programmatically in serve.js
    
    Co-authored-by: Peter Mallett <pmallett@redhat.com>
    Co-authored-by: Leo Zavala <lzavala@rehat.com>

--- tasks/serve.js
+++ tasks/serve.js
@@ -1,4 +1,5 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+
 const app = new Application();
 const router = new Router();
 
@@ -16,7 +17,51 @@ app.use(async (ctx, next) => {
   }
 });
 
+const makeIndexHtml = (list) => `
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Component Demos</title>
+  <meta name="description" content="Links to components/*/demo.">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+  <h1>Component Demos:</h1>
+  <ul>
+    ${list}
+  </ul>
+</body>
+</html>`;
+
 router
+  .get("/", async (ctx) => {
+    const path = "./components";
+
+    let list = "";
+    for await (const dirEntry of Deno.readDir(path)) {
+      if (dirEntry.isDirectory) {
+        const name = dirEntry.name;
+        const demoIndex = `${path}/${name}/demo/index.html`;
+        try {
+          await Deno.readFile(demoIndex);
+          list += `<li><a href="${name}/demo">${name}</a></li>`;
+        } catch (_unused) {
+          list += `<li>${name} (no demo)</li>`;
+        }
+      }
+    }
+
+    ctx.response.body = makeIndexHtml(list);
+    ctx.response.type = "text/html";
+  })
+  .get("/favicon.ico", async (ctx) => {
+    await ctx.send({
+      root: Deno.cwd() + "/assets",
+    });
+  })
   .get("/data/:file", async (ctx) => {
     if (ctx?.params?.file) {
       const data = await Deno.open(`./data/${ctx?.params?.file}`);
@@ -25,20 +70,26 @@ router
     }
   })
   .get("/docs/:component", (ctx) => {
-
-  })
+    // todo implement
+  });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
 app.use(async (ctx) => {
   await ctx.send({
-    root: Deno.cwd()+'/components',
-    index: 'index.html'
+    root: Deno.cwd() + "/components",
+    index: "index.html",
   });
 });
 
-if (Deno.args.indexOf('https') >= 0) {
-  await app.listen({ port: 8443, secure: true, certFile:'./localhost.pem', keyFile:'./localhost-key.pem'});
+if (Deno.args.indexOf("https") >= 0) {
+  await app.listen({
+    port: 8443,
+    secure: true,
+    certFile: "./localhost.pem",
+    keyFile: "./localhost-key.pem",
+  });
 } else {
-  await app.listen({ port: 8080, hostname: '0.0.0.0' });
+  await app.listen({ port: 8080, hostname: "0.0.0.0" });
 }
