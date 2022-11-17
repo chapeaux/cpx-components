e17d6f1fa8644d7ba65edfda8122ea71afefcee7
Leo Zavala <lzavala@rehat.com>
2022-11-17 15:44:49 -0800

    test(cpx-url): Adds test ensuring params-ready is emitted.

--- components/cpx-url/test/cpx-url_spec.js
+++ components/cpx-url/test/cpx-url_spec.js
@@ -30,6 +30,27 @@ describe("CPXUrl", () => {
     expect(ele.qty).to.equal(qty.toString());
   });
 
+  it("emits the params-ready event", async () => {
+    var event_emitted = false;
+    var emitted_data = {};
+
+    document.addEventListener("params-ready", (e) => {
+      emitted_data = e.detail;
+      event_emitted = true;
+    });
+
+    const ele = await fixture(html`
+        <cpx-url></cpx-url>
+    `);
+
+    await elementUpdated(ele);
+    expect(event_emitted).to.be.true;
+    expect(emitted_data.term).to.be.null;
+    expect(emitted_data.filters).to.exist;
+    expect(emitted_data.sort).to.equal("relevance");
+    expect(emitted_data.qty).to.be.null;
+  });
+
   it("cannot set an attribute that is not observable", async () => {
     const filters = "foo~bar";
     const ele = await fixture(html`
@@ -55,11 +76,11 @@ describe("CPXUrl", () => {
      *  }
      * }
      */
+  });
 
-    const ele = await fixture(html`
-        <cpx-url></cpx-url>
-    `);
-
-    await elementUpdated(ele);
+  it.skip("listens to search complete event", async () => {
+    /**
+     * @todo: test the search-complete event listener.
+     */
   });
 });
